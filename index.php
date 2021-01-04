7046a5eb0e982c7dea5b4f06f33b8df427fa5052
Admin <Admin@192.168.30.23>
2021-01-04 14:58:47 +0530

    kk

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>TODO write content</div>
+        <?php
+       $title="Welcome to bombay";
+       
+       $name ="Narendra";
+       $number=102;
+              
+       echo $name .$number;
+        ?> 
+        
+        <h1> <?php echo $title; ?> </h1>
+    </body>
+</html>
